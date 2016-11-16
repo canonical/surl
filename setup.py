0e99fa9a53af03cf2b9f8462d90fe5755798c05a
Celso Providelo <celso.providelo@gmail.com>
2016-11-16 16:30:58 -0200

    Enabling testing and snap CI to edge channel.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='surl',
@@ -13,4 +13,5 @@ setup(
         "requests",
         "pymacaroons",
     ],
+    test_suite='tests',
 )
