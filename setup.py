13cc3dc8fa776f90a121079a59180a4ade579f64
Daniel Manrique <roadmr@ubuntu.com>
2020-12-16 20:51:43 +0000

    Fix version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='surl',
-    version='0.72',
+    version='0.7.2',
     author='Celso Providelo',
     author_email='celso.providelo@canonical.com',
     url="https://github.com/cprov/surl",
