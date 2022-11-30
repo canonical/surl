8d87bd2f02c611832702d4b90282cbd9841107b2
Shane Pelletier <shane.pelletier@canonical.com>
2022-11-30 09:51:55 -0400

    Revert "Remove default headers"
    This reverts commit 56f3c02f5706b051bc1b864393419cc61b604171.

--- surl/__init__.py
+++ surl/__init__.py
@@ -30,6 +30,14 @@ __all__ = [
     "main",
 ]
 
+
+DEFAULT_HEADERS = {
+    "User-Agent": "surl/{}".format(os.environ.get("SNAP_VERSION", "devel")),
+    "Accept": "application/json, application/hal+json",
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+}
+
 CONSTANTS = {
     "local": {
         "sso_location": os.environ.get("SURL_SSO_LOCATION", "login.staging.ubuntu.com"),
@@ -426,6 +434,7 @@ def main():
     except CliDone:
         return 0
 
+    headers = DEFAULT_HEADERS.copy()
     if url is None:
         if config.store_type == "snapcraft":
             url = "{}/api/v2/tokens/whoami".format(
@@ -437,17 +446,15 @@ def main():
             )
 
     auth_header = get_authorization_header(config.root, config.discharge)
+    headers.update(auth_header)
 
     # -s hides progress bar and errors, -S brings the errors back, -L follows
     # redirects, and --output - prints binary output to terminal
-    arguments = [
-        "curl",
-        "-sSL",
-        "--output",
-        "-",
-        "-H",
-        f"Authorization: {auth_header['Authorization']}",
-    ]
+    arguments = ["curl", "-sSL", "--output", "-"]
+
+    for header, value in headers.items():
+        arguments.append("-H")
+        arguments.append(f"{header}: {value}")
 
     arguments.extend(remainder)
     arguments.append(url)
